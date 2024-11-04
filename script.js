f87c2eaacbc1b338f0c962f8717409e55308d3c0
Noel-shane-25 <jsnsancia@gmail.com>
2024-11-04 23:45:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("openLinkButton").addEventListener("click", function() {
+    window.open("https://drive.google.com/file/d/1etsOrmyqKr8a-nXvZ0SdOyfsOsyaDVJp/view?usp=sharing", "_blank");
+});
